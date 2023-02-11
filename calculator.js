b164d117a51a58bbc33dc021fc3a00cf8678b9d9
Matheus Rodrigues Marino <theusrodrino2104@gmail.com>
2023-02-11 19:26:36 -0300

    Correção

--- calculator.js
+++ calculator.js
@@ -11,7 +11,6 @@ document.querySelectorAll('.charKey').forEach(function(charKeyBtn){
     charKeyBtn.addEventListener('click', function(){
         const value = charKeyBtn.dataset.value
         input.value += value
-        input.focus()
     })
 })
 
